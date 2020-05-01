dece114a6c8fa68be333776ac465a21e8428d590
Sergey Ivaykin <serjvanilla@users.noreply.github.com>
2020-05-02 01:03:41 +0300

    typed error

--- response_test.go
+++ response_test.go
@@ -2,7 +2,6 @@ package fastjsonrpc_test
 
 import (
 	"testing"
-	"testing/iotest"
 
 	. "github.com/serjvanilla/fastjsonrpc"
 	"github.com/valyala/fasthttp"
@@ -27,30 +26,6 @@ func TestResponseWithError(t *testing.T) {
 	if !assertJSONUnordered(
 		t, string(ctx.Response.Body()),
 		`{"jsonrpc":"2.0","error":{"code":-32603,"message":"Internal error"},"id":1}`,
-	) {
-		t.Fatalf("unexpected response body: `%s`", ctx.Response.Body())
-	}
-}
-
-func TestResponseWithStdError(t *testing.T) {
-	r := NewRepository()
-	r.Register("ping", func(ctx *RequestCtx) {
-		ctx.SetError(iotest.ErrTimeout)
-	})
-
-	ctx := new(fasthttp.RequestCtx)
-	ctx.Request.Header.SetMethod(fasthttp.MethodPost)
-	ctx.Request.SetBodyString(`{"jsonrpc":"2.0","method":"ping","id":1}`)
-
-	r.RequestHandler()(ctx)
-
-	if ctx.Response.StatusCode() != fasthttp.StatusOK {
-		t.Fatal("unexpected status code")
-	}
-
-	if !assertJSONUnordered(
-		t, string(ctx.Response.Body()),
-		`{"jsonrpc":"2.0","error":{"code":-32000,"message":"Server error","data":"timeout"},"id":1}`,
 	) {
 		t.Fatalf("unexpected response body: `%s`", ctx.Response.Body())
 	}
